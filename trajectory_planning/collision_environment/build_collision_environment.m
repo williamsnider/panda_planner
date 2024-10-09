56aef25198e5016d9450029da49583469b5568bb
William Snider <williamgraysonsnider@gmail.com>
2024-10-09 14:01:52 -0400

    Revert "Adjust for 0.73" to return to 20240820 state.
    This reverts commit b3cbc56bd8a2d5b246563b430be4859d5d4a0f28.

--- trajectory_planning/collision_environment/build_collision_environment.m
+++ trajectory_planning/collision_environment/build_collision_environment.m
@@ -82,7 +82,7 @@ collision_objects{end+1} = box;
 
 %% Monkey reaching box
 box_dims_m = [0.01, 0.5, 0.5];
-box_trans_m = [-0.60, 0.08, 0.6477] + [-0.02-0.125, 0.0, 0.0];
+box_trans_m = [-0.57, 0.08, 0.6477] + [-0.02-0.125, 0.0, 0.0];
 box = collisionBox(box_dims_m(1), box_dims_m(2), box_dims_m(3));
 T = trvec2tform(box_trans_m);
 box.Pose = T;
